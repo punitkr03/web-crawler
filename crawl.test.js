c38d3bc55c94031edeae4bcec75586609e3d0ea0
tagpunitkr <tagpunitkumar@gmail.com>
2023-06-26 19:52:33 +0530

    normaliseURL

---
+++ crawl.test.js
@@ -0,0 +1,30 @@
+const {normalizeUrl} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+test('normalizeUrl strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeUrl(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeUrl(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl  capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeUrl(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl  strip http', () => {
+    const input = 'http://BLOG.boot.dev/path/'
+    const actual = normalizeUrl(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
