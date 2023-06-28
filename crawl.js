3fb6c799f3b4ab6fca7218825e46c53442407e71
tagpunitkr <tagpunitkumar@gmail.com>
2023-06-28 19:25:00 +0530

    Crawl page recursive added

--- crawl.js
+++ crawl.js
@@ -1,23 +1,40 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(currentURL, htmlBody) {
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if(baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+    const normalizedCurrentURL = normalizeUrl(currentURL)
+    if(pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
     console.log(`Crawling ${currentURL}`)
+
     try {
         const res = await fetch(currentURL)
         if(res.status > 399) {
             console.log(`Error in fetch with status code: ${res.status}, on page: ${currentURL}`)
-            return
+            return pages
         }
         const contentType = res.headers.get('content-type')
         if(!contentType.includes("text/html")) {
             console.log(`Non html response, content-type: ${contentType}, on page: ${currentURL}`)
-            return
+            return pages
         }
-
-        console.log(await res.text())
+        const htmlBody = await res.text()
+        const urls = getURLsFromHTML(htmlBody, baseURL)
+        for(const url of urls) {
+            await crawlPage(baseURL, url, pages)
+        }
     } catch(err) {
         console.log(`Error in fetch: ${err.message}, on page: ${currentURL}`)
     }
+    return pages
 }
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
